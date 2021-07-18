804acaee3ae5ce1d0008bfcd2cf0c6d95c466c13
Jon <8bityamist@gmail.com>
2021-07-18 13:27:13 -0700

    Working on inventory system, including items and equipment.

---
+++ Assets/Scripts/DragItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private RectTransform dragRectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    public GameObject itemSlot;
+
+    private void Awake() {
+        dragRectTransform = GetComponent<RectTransform>();
+        canvas = FindObjectOfType<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        canvasGroup.alpha = .65f;
+        canvasGroup.blocksRaycasts = false;
+    }
+    public void OnDrag(PointerEventData eventData) {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        if (itemSlot) {
+            GetComponent<RectTransform>().anchoredPosition = itemSlot.GetComponent<RectTransform>().anchoredPosition;
+            itemSlot.GetComponent<ItemSlot>().attachedItem = gameObject;
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public string attachedCharacter; // TODO: Change to characterclass
+    private Vector2 centerPos;
+    public GameObject attachedItem; // TODO: Change this to an item.
+
+    public bool acceptsItems = true;
+    public bool acceptsUsable = false;
+    public bool acceptsKey = false;
+    public bool acceptsEquipment = false;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        centerPos = transform.position;
+    }
+
+    public void OnDrop(PointerEventData eventData) {
+        if (eventData.pointerDrag != null) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            attachedItem = eventData.pointerDrag.gameObject;
+            if (eventData.pointerDrag.GetComponent<DragItem>().itemSlot != null)
+                eventData.pointerDrag.GetComponent<DragItem>().itemSlot.GetComponent<ItemSlot>().attachedItem = null;
+            eventData.pointerDrag.GetComponent<DragItem>().itemSlot = gameObject;
+        }
+    }
+
+    public bool hasItem() {
+        return attachedItem != null;
+    }
+
+    // get item
+
+
+}
