5b2ab26ff5b18b227c6e4b36b0979c7b4a1af5d6
Jon <8bityamist@gmail.com>
2021-07-18 00:23:49 -0700

    First commit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    Vector2 startPos;
+    private void Awake() {
+        startPos = transform.position;
+    }
+
+    public void ToggleEnabled() {
+        transform.position = startPos;
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
